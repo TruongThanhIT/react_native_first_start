136b4fb5db674cafd5d4119b04393a4ce9f2968a
Truong Huynh Thuy Thai Thanh <thanh.truonghuynhthuythai@hotmail.com>
2025-12-22 14:51:20 +1300

    Platform

--- App.tsx
+++ App.tsx
@@ -1,71 +1,17 @@
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import {
   StyleSheet,
-  View,
-  Image,
-  Button,
-  Alert,
-  TouchableOpacity,
-  Pressable,
-  ScrollView,
+  Text,
+  Platform
 } from "react-native";
 
 export default function App() {
-  const onButtonPress = () => Alert.alert("Button pressed!");
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <ScrollView 
-        showsVerticalScrollIndicator={false}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={
-          {
-            backgroundColor:"red"
-          }
-        }
-        >
-          <TouchableOpacity onPress={onButtonPress}>
-            <Image
-              source={require("./assets/splash-icon.png")}
-              style={styles.image1}
-            />
-          </TouchableOpacity>
-
-          <Pressable onPress={onButtonPress}>
-            <Image
-              source={{ uri: "https://picsum.photos/200/300" }}
-              style={styles.image2}
-              blurRadius={2}
-            />
-          </Pressable>
-
-          <Pressable onPress={onButtonPress}>
-            <Image
-              source={{ uri: "https://picsum.photos/200/300" }}
-              style={styles.image2}
-              blurRadius={2}
-            />
-          </Pressable>
-
-          <Pressable onPress={onButtonPress}>
-            <Image
-              source={{ uri: "https://picsum.photos/200/300" }}
-              style={styles.image2}
-              blurRadius={2}
-            />
-          </Pressable>
-
-          <Pressable onPress={onButtonPress}>
-            <Image
-              source={{ uri: "https://picsum.photos/200/300" }}
-              style={styles.image2}
-              blurRadius={2}
-            />
-          </Pressable>
-
-          <Button title="Press on me" onPress={onButtonPress} />
-        </ScrollView>
+        <Text>
+          "This is an {Platform.OS === "android" ? "Android" : "iOS"} device"
+        </Text>
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -74,15 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
-  },
-  image1: {
-    width: 200,
-    height: 200,
-  },
-  image2: {
-    width: 200,
-    height: 200,
-    borderRadius: 100,
+    backgroundColor: Platform.OS === "ios" ? "grey" : "aqua",
+    justifyContent:"center"
   },
 });
