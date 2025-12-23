a8535ef7e2636c3ade84b0abf9c1d1a4e46f4763
Truong Huynh Thuy Thai Thanh <thanh.truonghuynhthuythai@hotmail.com>
2025-12-23 15:20:00 +1300

    Flex used for seperate part view

--- App.tsx
+++ App.tsx
@@ -1,44 +1,31 @@
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
-import { StyleSheet, ActivityIndicator, View, Text } from "react-native";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <ActivityIndicator size={"large"} color={"green"} />
-        <ActivityIndicator />
-        <View style={styles.view1}>
-          <Text style={styles.text1}>Hello</Text>
-          <View style={styles.childView}>
-
-          </View>
-        </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <View
+      style={{
+        backgroundColor: "white",
+        flex: 1,
+      }}>
+      <View
+        style={{
+          backgroundColor:"red",
+          flex: 2,
+        }}
+      ></View>
+      <View
+        style={{
+          backgroundColor:"green",
+          flex: 1,
+        }}
+      ></View>
+      <View
+        style={{
+          backgroundColor:"blue",
+          flex: 1,
+        }}
+      ></View>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    justifyContent: "center",
-  },
-  view1: {
-    height: 200,
-    width: 200,
-    backgroundColor: "red",
-    alignItems: "center",
-    marginTop: 10,
-    marginLeft: 10,
-    paddingTop: 40,
-  },
-  text1: {
-    fontSize: 50,
-  },
-  childView: {
-    height:50,
-    width:50,
-    backgroundColor:"green"
-  }
-});
