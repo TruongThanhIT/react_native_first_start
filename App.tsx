1c406c28586a133318cee2d0361075d79d232184
Truong Huynh Thuy Thai Thanh <thanh.truonghuynhthuythai@hotmail.com>
2025-12-29 10:43:43 +1300

    flexDirection

--- App.tsx
+++ App.tsx
@@ -1,31 +1,25 @@
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
-import { View } from "react-native";
+import { View, Button, Text } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [state, setState] = useState(20)
+  const IncreaseValue = () => setState(prevState => prevState + 1)
+  const DecreaseValue = () => setState(prevState => prevState - 1)
   return (
     <View
       style={{
         backgroundColor: "white",
         flex: 1,
+        flexDirection: "column", // if column main justify content will be vertical, else for row
+        justifyContent: "center", // for vertical
+        alignItems: "center" // for horizontal
       }}>
-      <View
-        style={{
-          backgroundColor:"red",
-          flex: 2,
-        }}
-      ></View>
-      <View
-        style={{
-          backgroundColor:"green",
-          flex: 1,
-        }}
-      ></View>
-      <View
-        style={{
-          backgroundColor:"blue",
-          flex: 1,
-        }}
-      ></View>
+     <Button title="Increase" onPress={IncreaseValue}/>
+      <Text style = {{fontSize:40}}>
+        {state}
+      </Text>
+     <Button title="Decrease" onPress={DecreaseValue}/>
     </View>
   );
 }
