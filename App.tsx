65e832be3632bd32925f79a65e7bffa2ccf1a1e3
Truong Huynh Thuy Thai Thanh <thanh.truonghuynhthuythai@hotmail.com>
2025-12-30 19:56:17 +1300

    Background

--- App.tsx
+++ App.tsx
@@ -1,18 +1,32 @@
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
-import { View, Button, Text, Modal } from "react-native";
+import { View, Button, Text, Modal, ImageBackground } from "react-native";
 import { useState } from "react";
 import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function App() {
   const [modalVisible, setModalVisible] = useState(false)
   return (
-    <SafeAreaView style={{backgroundColor:"gold", flex:1, justifyContent:"center"}}>
-      <Button title="Open the Modal" onPress={() => setModalVisible(true)}/>
-      <Modal visible={modalVisible} animationType="slide">
-        <Text style={{fontSize:50, marginTop:20}}>This is a Modal</Text>
-        <Button title="Close the Modal" onPress={() => setModalVisible(false)}/>
-          <AntDesign name="close-circle" size={42} color="red" onPress={() => setModalVisible(false)}/>
-      </Modal>
-    </SafeAreaView>
+    <ImageBackground
+    source={require("./assets/background.jpg")}
+    style={{
+      backgroundColor: "gold",
+      flex: 1,
+      justifyContent:"center",
+      alignItems:"center"
+    }}
+    >
+      <View 
+      style={{
+        backgroundColor: "#fff",
+        height: 200,
+        width: 200,
+        borderRadius: 15
+      }} >
+
+        <Text>Hello world!</Text>
+
+      </View>
+
+    </ImageBackground>
   );
 }
