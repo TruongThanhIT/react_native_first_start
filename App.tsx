396f1f84a10f3c64336eb74c6301833b3479cffb
Truong Huynh Thuy Thai Thanh <thanh.truonghuynhthuythai@hotmail.com>
2025-12-30 20:09:26 +1300

    Input text

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
-import { View, Button, Text, Modal, ImageBackground } from "react-native";
+import { View, Button, Text, Modal, ImageBackground, TextInput } from "react-native";
 import { useState } from "react";
 import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function App() {
   const [modalVisible, setModalVisible] = useState(false)
+  const [text, setText] = useState("")
+  console.log("User type: ", text);
   return (
     <ImageBackground
     source={require("./assets/background.jpg")}
@@ -24,8 +26,29 @@ export default function App() {
       }} >
 
         <Text>Hello world!</Text>
+        <TextInput
+        style={{
+          height: 40,
+          width: "80%",
+          borderRadius: 4,
+          borderWidth: 1,
+          borderColor: "grey"
+        }}
+        placeholder="Input here"
+        keyboardType="default"
+        //secureTextEntry={true}
+        editable={true}
+        // multiline={true}
+        //Control the text
+        value= {text}
+        onChangeText={setText}
+        >
+
+        </TextInput>
+        <Text>My name is: {text}</Text>
 
       </View>
+
 
     </ImageBackground>
   );
