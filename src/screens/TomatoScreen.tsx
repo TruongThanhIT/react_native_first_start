2451c081cb9377cdc5676a86601e7191789c6b52
Truong Huynh Thuy Thai Thanh <thanh.truonghuynhthuythai@hotmail.com>
2026-01-03 14:58:44 +1300

    Show list book from mock api

--- src/screens/TomatoScreen.tsx
+++ src/screens/TomatoScreen.tsx
@@ -1,22 +1,24 @@
-import { StyleSheet, Text, View, Button } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Button, FlatList, Image } from "react-native";
+import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import axios from "axios";
 
 const TomatoScreen = () => {
+  const [bookList, setBookList] = useState([]);
   const navigation = useNavigation();
   const endpointURL = "https://69586d4a6c3282d9f1d4ff8a.mockapi.io/books";
   const getListOfBooks = async () => {
     const response = await axios.get(endpointURL);
     console.log(JSON.stringify(response.data, null, 3));
+    setBookList(response.data);
   };
+  console.log("====================bookList=============================");
+  console.log(bookList);
+  console.log("=================================================");
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>TomatoScreen Screen</Text>
-      <Button 
-      title="Load books" 
-      onPress={getListOfBooks}
-      />
       <Button
         title="Go to Gold Screen"
         onPress={() => navigation.navigate("GoldScreen")}
@@ -27,6 +29,22 @@ const TomatoScreen = () => {
       <Text onPress={() => navigation.navigate("GoldScreen", { name: "Joe" })}>
         Name: Joe
       </Text>
+      <Button title="Load books" onPress={getListOfBooks} />
+      <FlatList
+        data={bookList}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.name_of_author}</Text>
+            <Text>{item.price_of_book}</Text>
+            <Image source={{uri: item.cover}}
+            style={{
+              height:150,
+              width: 150,
+            }} ></Image>
+          </View>
+        )}
+      />
     </View>
   );
 };
